ac90194da5225e0ac2f78cd46b4e13e6d5b7e2a1
“Tyler <tylerpalef@gmail.com>
2018-07-19 13:53:42 -0400

    finished string and array

--- research.rb
+++ research.rb
@@ -11,12 +11,26 @@
       puts ""
 
   # strip
+    #Removes extra spaces on a string
+      strip = "       look at me strip        "
+      puts strip.strip
+      puts ""
+
   # split
-    # Will seperate a string itno an array
+    # Will seperate a string into an array
+      example_split = "string to array"
+      puts example_split.split
+      puts ""
+
   # start_with?
+    # returns true if one of the prefixes matches
+    puts "hello".start_with?("heaven", "hello")
+    puts ""
+    puts "hello".start_with?("fart", "poop")
+    puts ""
 
   # "Array"
-    #Example Array
+    # Example Array
     example_array = ["apple", "orange", "cherry", "grapes", "lime", "kiwi"]
   ############################
   # first
@@ -24,14 +38,21 @@
       puts "Array - first: "
       puts example_array.first
       puts ""
+
   # delete_at
     # Removes a specified number from the array
+      puts "Array - delete_at"
+      example_array.delete_at(2)
+      puts example_array
+      puts ""
+      
   # delete
     # Will delete the specified object in the array. Need to enter object name
       puts "Array - delete: "
       example_array.delete("apple")
       puts example_array
       puts ""
+
   # pop
     # pop will remove the last item on the array
       puts "Array - pop: "
