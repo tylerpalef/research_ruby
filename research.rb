29d9b9d95192e9fc9d148cbf18f822f78e75ce18
“Tyler <tylerpalef@gmail.com>
2018-07-19 12:03:44 -0400

    started research

---
+++ research.rb
@@ -0,0 +1,54 @@
+# Research
+
+# "String"
+############################
+  # length
+    # length will determine how many characters are in your string
+    # Example:
+      puts "string - length: "
+      tyler = "this is a string"
+      puts tyler.length
+      puts ""
+
+  # strip
+  # split
+    # Will seperate a string itno an array
+  # start_with?
+
+  # "Array"
+    #Example Array
+    example_array = ["apple", "orange", "cherry", "grapes", "lime", "kiwi"]
+  ############################
+  # first
+    # will return the first item in the array
+      puts "Array - first: "
+      puts example_array.first
+      puts ""
+  # delete_at
+    # Removes a specified number from the array
+  # delete
+    # Will delete the specified object in the array. Need to enter object name
+      puts "Array - delete: "
+      example_array.delete("apple")
+      puts example_array
+      puts ""
+  # pop
+    # pop will remove the last item on the array
+      puts "Array - pop: "
+      example_array.pop
+      puts example_array
+
+# "Hash"
+############################
+# to_a
+# has_key?
+# has_value?
+
+# "Time"
+############################
+# now
+
+# File
+############################
+# exist?
+# extname
